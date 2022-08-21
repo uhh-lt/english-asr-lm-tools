73bade9117195527f1be756229eac6e9a3d8a122
Alienmaster <robert.geislinger@uni-hamburg.de>
2022-08-21 14:30:04 +0200

    Preprocess Data Steps

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ A toolset for german can be found: https://github.com/bmilde/german-asr-lm-tools
 These tools work best with the Pile dataset: https://pile.eleuther.ai/
 
 ## Requirements
+At least one (or more!) terabyte of free space because the Pile is decompressed 825GB large and some of the following steps create copys of the dataset.
+
 ZST library to decompress the Pile
 ```
 sudo apt install zstd
 ```
-Create an virtual environment
+Create and start a virtual environment
 ```
 virtualenv -p python3.9 env
 source env/bin/active
@@ -23,4 +25,13 @@ At first download and decompress the Pile:
 ```
 
 The 30 parts are now decompressed in the `pile` folder.
+
+## Sort the files
+To run the scripts we need to unmix the files at first.
+in `convertPile.py` in Line 10 you can define the needed datasources.
+The unmixed data is stored into the pileSorted folder.
+To sort the files start
+```
+python convertPile.py
+```
 
---
+++ convertPile.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+sourceFolder = 'pile'
+destinationFolder = 'pileSorted'
+
+# {'ArXiv', 'BookCorpus2', 'Books3', 'DM Mathematics', 'Enron Emails', 'EuroParl', 'FreeLaw', 'Github', 'Gutenberg (PG-19)', 'HackerNews', 'NIH ExPorter', 'OpenSubtitles', 'OpenWebText2', 'Pile-CC', 'PhilPapers', 'PubMed Central', 'PubMed Abstracts', 'StackExchange', 'Ubuntu IRC', 'USPTO Backgrounds', 'Wikipedia (en)', 'YoutubeSubtitles'}
+
+# The parts of the dataset which are saved into new files
+keep = ['EuroParl', 'YoutubeSubtitles', 'Wikipedia (en)']
+
+if not os.path.exists(destinationFolder):
+    os.makedirs(destinationFolder)
+
+for a in range(0,30):
+    filename = f'{a:02d}.jsonl'
+    print(f'Reading {filename}')
+    sentencedict = {}
+
+    with open(f'{sourceFolder}/{filename}', 'r') as json_file:
+        json_list = list(json_file)
+
+    print(f'Sorting {filename}')
+
+    for json_str in json_list:
+        result = json.loads(json_str)
+        source = result['meta']['pile_set_name']
+        result = result['text']
+
+        if source in keep:
+            if sentencedict.get(source):
+                sentencedict[source].append(str(result + '\n'))
+            else:
+                sentencedict[source] = [str(result)]
+
+    print(f'Writing sorted data from {filename}')
+
+    for meta in sentencedict:
+        open(f'{destinationFolder}/{meta}_{a}', 'a').write(str(sentencedict[meta]))
+
+    print(f'Finished processing {filename}')
+
+print('All files are processed')
