cefda69c4630d407de00646cb55165d4cccb806c
Alienmaster <robert.geislinger@uni-hamburg.de>
2022-08-24 13:32:03 +0200

    Update Readme and YT Processing

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 These tools can be used to prepare texts for asr tasks.
 A toolset for german can be found: https://github.com/bmilde/german-asr-lm-tools/
 
-These tools work best with the Pile dataset: https://pile.eleuther.ai/
+These tools work best with [the Pile](https://pile.eleuther.ai/) dataset and the officals [wikipedia dumps](https://dumps.wikimedia.org/enwiki/latest/).
 
 ## Requirements
 At least one (or more!) terabyte of free space because the Pile is decompressed 825GB large and some of the following steps create copys of the dataset.
-Due to the multiprocessing plenty a lot of RAM and CPUs are also recommended.
+Due to the multiprocessing plenty of RAM and CPUs are also recommended.
 
 ZST library to decompress the Pile
 ```
@@ -21,12 +21,16 @@ pip install -r requirements.txt
 python -m spacy download en_core_web_sm
 ```
 ## Download and decompress Files
-At first download and decompress the Pile:
+At first download and decompress the Pile. This dataset is used for the Youtube Subtitles:
 ```
 ./downloadPile.sh
 ```
-
 The 30 parts are now decompressed in the `pile` folder.
+
+To process wikipedia articles the latest dump is used:
+```
+wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2
+```
 
 ## Sort the files
 To run the scripts we need to unmix the files at first.
@@ -36,4 +40,13 @@ To sort the files start
 ```
 python convertPile.py
 ```
-
+## Youtube Subtitles
+To prepare Youtube Subtitles run the script:
+```
+python ytcleanup.py -o ytClean -n 30
+```
+`-o` sets the output folder (eg ytClean), `-n` sets the number of processes and `-p` preserves punctuation if needed.
+The resulting files can be concatenated to one file.
+```
+cat ytClean/* > YouTubeClean
+```
--- ytcleanup.py
+++ ytcleanup.py
@@ -5,27 +5,23 @@ from spacy.lang.en import English
 import spacy_fastlang
 import multiprocessing
 import os
-import re
+import argparse
 
 punctuation = False
 
-folder = 'lb/'
+InputFolder = 'pileSorted/'
 
-processes = 30 # Number of processes
-
-nlp = spacy.load("en_core_web_sm")
-nlp.add_pipe("language_detector")
+nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe('language_detector')
 
 # Tokenizer
 nlpT = English()
 tokenizer = nlpT.tokenizer
 
-
 def readFile(filename):
     print('read file')
-    with open(f'{folder}{filename}', 'r') as f:
+    with open(f'{InputFolder}{filename}', 'r') as f:
         subtitles = f.read()
-    # subtitles = '["Hey everybody I wanted to give a little plug real quick.'
     subtitles = subtitles.split('\n')
     return subtitles
 
@@ -33,8 +29,7 @@ def firstStage(subtitles):
     temp = []
     print('First stage')
     drop = False
-    for line in subtitles: # Try to remove lines with [ ] and blocks of text with languages other than english
-        # print(line)
+    for line in subtitles: # Try to remove lines with special characters and blocks of text with languages other than english
         if not drop:
             if any(x in line for x in ('Arabic:', 'Bulgarian:', 'Chinese:', 'Czech:', 
                                         'Danish:', 'Dutch:', 'Finnish:', 'French:', 'German:', 
@@ -48,26 +43,25 @@ def firstStage(subtitles):
             if line:
                 if any(x in line for x in ('English:', '(audience', '(sighs)', '/', '\\', 'www.', '.edu', '.com', '.org', '*', '(music)')):
                     continue
-                if (line == "music"):
+                if (line == 'music'):
                     continue
-                if any(x in line for x in ('[', ']', '"', ')', '(', '%', '&', '=', '`', '#', '+', '~', '<', '|', '}', '{', '^', )):
+                if any(x in line for x in ('[', ']', '"', ')', '(', '%', '&', '=', '`', '#', '+', '~', '<', '|', '}', '{', '^')):
                     continue
                 if line.isspace():
                     continue
                 if line.isupper():
                     continue
-                if any(c.isalpha() for c in line):
+                if any(c.isalpha() for c in line): # remove the gaps between punctuation and the word
                     lineC = line.replace('", "', '').replace('", \'', '').replace('\', "', '').replace('-', '').replace('\', \'', '').replace('_', '').replace('>>', '').replace('  ', ' ').replace(';', '').replace("', '", "")
                     lineC = cleanupTools.rulesForCommonWords(lineC)
                     lineC = lineC.lstrip()
                     temp.append(lineC)
-                
         else:
-            if (line.isspace()) or (line =="English:") or (line.startswith("', '")):
+            if (line.isspace()) or (line =='English:') or (line.startswith("', '")):
                 drop = False
             else:
                 continue
-    print("finished sort part one")
+    print('Finished stage one')
     return temp
 
 def secondStage(subtitles):
@@ -100,12 +94,12 @@ def thirdStage(subtitles): # Detect the rest of the sentences with spacy
         if (result._.language == 'en') and (result._.language_score >= 0.8):
             tline = line
             tokens = tokenizer(tline)
-            
+
             tline = normalizer.normalization(tokens)
 
             if not any(x for x in tline if x.isnumeric()):
                 tline = cleanupTools.replaceShortforms(tline)
-                tline = cleanupTools.punctuation(tline, False)
+                tline = cleanupTools.punctuation(tline, punctuation)
             else:
                 print(tline)
                 continue
@@ -115,7 +109,7 @@ def thirdStage(subtitles): # Detect the rest of the sentences with spacy
 
 def writeToFile(subtitles, filename):
     # text = '\n'.join(str(elem).lower() for elem in subtitles)
-    with open('ytClean' + filename, 'w') as f:
+    with open(f'{output}/{filename}', 'w') as f:
         f.write(subtitles)
 
 def oneTurn(filename):
@@ -127,8 +121,20 @@ def oneTurn(filename):
     return subtitles
 
 if __name__ == '__main__':
-    files = [x for x in os.listdir(folder) if x.startswith('YoutubeSubtitles')]
-    pool = multiprocessing.Pool(processes)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output', type=str, required=False, default='ytClean', help='changes the destination folder')
+    parser.add_argument('-p', '--punctuation', type=bool, required=False, default=False, help='Remove(default) or preserve punctuation')
+    parser.add_argument('-n', '--number', type=int, required=False, default=30, help='Number of multiprocessing tasks')
+    args = parser.parse_args()
+    punctuation = args.punctuation
+    output = args.output
+    number = args.number
+
+    # Create output folder
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    files = [x for x in os.listdir(InputFolder) if x.startswith('YoutubeSubtitles')]
+    pool = multiprocessing.Pool(number)
     subtitles = pool.map(oneTurn, files)
     print(x for x in subtitles if len(x))
-    # writeToFile(subtitles)
